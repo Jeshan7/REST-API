f084b4d95e46026bd619372c30d889970e3f19cd
Jeshan <jeshankhn6@gmail.com>
2019-06-07 16:49:35 +0545

    PUT and DELETE options are now added to the RESTApi

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const express = require('express')
       mongoose = require("mongoose");
 
 var app = express();
+
 mongoose.connect("mongodb://localhost/driver");
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 app.use('/api', routes);
+app.use((err, req, res, next) => {
+  res.status(422).send({ERROR: "Name field is required"})
+})
 
 app.listen(process.env.port || 3000, ()=>{
   console.log("Connected ..");
--- routes/api.js
+++ routes/api.js
@@ -6,22 +6,28 @@ router.get('/driver', (req, res) => {
   res.send("hi");
 })
 
-router.post('/driver', (req, res) => {
+router.post('/driver', (req, res, next) => {
 
   Driver.create(req.body).then((data) => {
     res.send({type: "POST", Data: data});
-  })
+  }).catch(next);
 
   // var driver = new Driver(req.body);
   // driver.save();
 })
 
 router.put('/driver/:id', (req, res) => {
-  res.send("hi");
+  Driver.findByIdAndUpdate({_id: req.params.id}, req.body).then(() => {
+    Driver.findOne({_id: req.params.id}).then((data) => {
+      res.send(data);
+    })
+  })
 })
 
-router.delete('/driver/:id', (req, res) => {
-  res.send("hi");
+router.delete('/driver/:id', (req, res, next) => {
+  Driver.findByIdAndRemove({_id: req.params.id}).then((data) => {
+    res.send(data)
+  });
 })
 
 module.exports = router;
